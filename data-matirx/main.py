e7c9b1e6217b146dda6e551ef4feca64691b0943
xin <xinxinxin.space@gmail.com>
2024-07-26 08:50:38 +0800

    diamond-line-grain ver1.0

--- data-matirx/main.py
+++ data-matirx/main.py
@@ -11,23 +11,23 @@ import glob
 import cv2
 import time
 
-model = YOLO('../models/diamond-line-grain-9-yolov10x.pt')
+model = YOLO('../models/diamond-line-newly-yolov8x-300.pt')
 
-files = glob.glob('/Users/xin/Desktop/HIT/240719/线1/20240719180030150.bmp')
+files = glob.glob('/Users/xin/Desktop/HIT/240719/线6/20240719185447651.bmp')
 
 start_time = time.time()
 
 for file in files:
     image = cv2.imread(file)
-    result = model.predict(image)[0]
+    result = model.predict(image, conf=0.0067, iou=0.3, device='mps')[0]
     boxes = result.boxes
     for box in boxes:
         x1, y1, x2, y2 = box.cpu().xyxy[0]
         cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 3)
     cv2.imshow('image', image)
     key = cv2.waitKey(0)
-    # if key == ord('q'):
-    #     break
+    if key == ord('q'):
+        break
 
 end_time = time.time()
 
