741db89a27b65d4c9f894cba7bef082f147ed01f
xin <xinxinxin.space@gmail.com>
2024-07-26 08:55:13 +0800

    multi-platform support

--- diamond-line-grain/main.py
+++ diamond-line-grain/main.py
@@ -5,7 +5,7 @@
 @Author  ：xin700
 @Date    ：2024/7/15 11:24
 """
-
+import torch
 from ultralytics import YOLO
 import glob
 import cv2
@@ -13,13 +13,30 @@ import time
 
 model = YOLO('../models/diamond-line-newly-yolov8x-300.pt')
 
+
+def get_device():
+    if torch.cuda.is_available():
+        device = 'cuda'
+        print("CUDA is available. Using GPU.")
+    elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
+        device = 'mps'
+        print("MPS is available. Using MPS.")
+    else:
+        device = 'cpu'
+        print("CUDA and MPS are not available. Using CPU.")
+
+    return device
+
+
+device = get_device()
+
 files = glob.glob('/Users/xin/Desktop/HIT/240719/线6/20240719185447651.bmp')
 
 start_time = time.time()
 
 for file in files:
     image = cv2.imread(file)
-    result = model.predict(image, conf=0.0067, iou=0.3, device='mps')[0]
+    result = model.predict(image, conf=0.0067, iou=0.3, device=device)[0]
     boxes = result.boxes
     for box in boxes:
         x1, y1, x2, y2 = box.cpu().xyxy[0]
